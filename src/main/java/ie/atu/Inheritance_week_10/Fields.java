359c897bdb6f60e0ebb6a7d8dd3f5377c8f51c33
Rakib <G00408644>
2022-11-28 09:52:20 +0000

    Person created

---
+++ src/main/java/ie/atu/Inheritance_week_10/Fields.java
@@ -0,0 +1,15 @@
+package ie.atu.Inheritance_week_10;
+
+public class Fields {
+    public static void main(String[] args) {
+        Person dave = new Person();
+        dave.setName("Dave");
+        dave.setAddress("Mervue");
+        dave.setNumber("1234");
+
+        System.out.println("Here are daves detials"+ dave.getName());
+
+    }
+}
+
+
